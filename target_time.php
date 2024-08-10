cf6c71b828af89be19d8c5570aaf47bf886a31d4
MML0 <h1337124lat@gmail.com>
2024-08-10 16:44:45 +0430

    first

--- target_time.php
+++ target_time.php
@@ -1,6 +1,8 @@
 <?php
 date_default_timezone_set('Asia/Tehran'); // Set timezone to Iran
 
+// Add one hour to the current time
+$time = strtotime('+1 hour');
 $hours = date("H");
 $minutes = date("i") + 1; // Increment minutes by 1
 $seconds = date("s");
